362754e290af0bcddcf71a6f5b501f446526aa63
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-10-02 12:55:58 -0700

    Walkie Talkie chat

---
+++ src/ChatClient.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient
+{
+
+	public static void main(String[] args) 
+	{
+		String host = "127.0.0.1";
+		int port = 1234;
+		
+		try
+		{
+			Socket server = new Socket(host, port);
+			PrintWriter send = new PrintWriter(server.getOutputStream(), true);
+			BufferedReader recive = new BufferedReader
+									(new InputStreamReader(server.getInputStream()));
+			Scanner input = new Scanner(System.in);
+			
+			String message = "running";
+			while(!message.equals("Bye"))
+			{
+				System.out.println(recive.readLine());
+				message = input.nextLine();
+				send.println(message);
+			}
+			send.close();
+			recive.close();
+			server.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Connection failed");
+		}
+	}
+	
+}
---
+++ src/ChatServer.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatServer
+{
+
+	public static void main(String[] args)
+	{
+		int port = 1234;
+		String host = "127.0.0.1";
+		try
+		{
+			ServerSocket server = new ServerSocket(port);
+			Socket client = server.accept();
+			PrintWriter send = new PrintWriter(client.getOutputStream(), true);
+			BufferedReader recive = new BufferedReader
+									(new InputStreamReader(client.getInputStream()));
+			Scanner input = new Scanner(System.in);
+			
+			while(true)
+			{
+				try
+				{
+					send.println(input.nextLine());
+					System.out.println(recive.readLine());
+				}
+				catch(Exception e)
+				{
+					System.out.println("Waiting for client");
+					client = server.accept();
+					send = new PrintWriter(client.getOutputStream(), true);
+					recive = new BufferedReader
+											(new InputStreamReader(client.getInputStream()));
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Server failure");
+		}
+	}
+
+}
